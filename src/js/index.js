f2ed2e5ea934551a3d03df7b8bf0d15bf902db23
Heather Conley <hconley6695@gmail.com>
2016-04-01 11:56:32 -0400

    trying to go back to original game to build constructor

--- src/js/index.js
+++ src/js/index.js
@@ -3,30 +3,50 @@ import $ from 'jquery';
 import _ from 'lodash';
 
 import questions from './questionArray';
+	
+	// var imageOne = question.image; 
+	// console.log(imageOne);
 
-class Questions {
-	constructor (questions) {
-		this.question = questions;
-		console.log(this.question);
-		this.choices = questions.choices_for_answer;
-		this.label = choices.label;
-		this.correct = choices.rightOrWrong;
+class Question {
+	constructor (question) {
+		// this.question = question;
+		this.image = question.image;
+		// this.choices = questions.choices_for_answer;
+		// this.label = choices.label;
+		// this.correct = choices.rightOrWrong;
 	}
-	eachQuestion(questionParam) {
+	eachQuestion(qParam) {
 
-		var imageOne = questionParam.image[0];
-		console.log(imageOne);
-		var labelArray = this.choices.map(function(choice)	{
-			return choice.label;
+		var animalImage = this.image;
+		('.question_stage').append(`<img src=${animalImage}>`);
 
-		})
 
-		var buttons = labelArray.forEach(function(label) {
-			$('.question_stage').append(`<li> <button> ${label}</button></li>`);
+		//var imageOne = questionParam.questions[0].image;
 
-		})
+		// var labelArray = this.choices.map(function(choice)	{
+		// 	return choice.label;
+
+		// })
+
+		// var buttons = labelArray.forEach(function(label) {
+		// 	$('.question_stage').append(`<li> <button> ${label}</button></li>`);
+
+		// })
 
 	}
 
 
 }
+
+console.log(questions);
+
+var createQ = function(data){
+	var thingy = new Question(data);
+};
+
+var questArray = _.forEach(questions, createQ);
+console.log(questArray);
+
+// var qs = new Question (questions);
+
+// console.log(qs);
--- src/js/questionArray.js
+++ src/js/questionArray.js
@@ -1,43 +1,60 @@
-var questions = [
 
-		{
+
+var turtle = {
 		image: 'http://icons.iconarchive.com/icons/fixicon/farm/256/turtle-icon.png',
 		choices_for_answer: [ {label:'F', correct: false}, {label: 'L', correct: false}, {label:'R', correct: false}, {label: 'T', correct: true}]
 
-	},
-		{
+	}
+
+var elephant = 	{
 		image: 'http://cliparts.co/cliparts/rcL/xM8/rcLxM8ypi.png',
 		choices_for_answer: [ {label: 'E', correct: true}, {label: 'V', correct: false}, {label: 'M', correct: false}, {label: 'H', correct: false}]
 
-	},
-		{
+	}
+
+var zebra =	{
 		image: 'http://zebra-pictures.clipartonline.net/_/rsrc/1448047942245/zebra-images/cartoon_zebra_kicking.png',
 		choices_for_answer: [ {label:'N', correct: false}, {label:'Z', correct: true}, {label: 'V', correct: false}, {label: 'X', correct: false}]
 
-	},
-		{
+	}
+
+var lion =	{
 		image: 'http://ap.lanexdev.com/user_images/Discovery/image/magazine/2015/01/Lion%20Cartoon%20dreamstime_45671319.png',
 		choices_for_answer: [ {label:'Q', correct: false}, {label: 'L', correct: true}, {label: 'P', correct: false}, {label: 'I', correct: false}]
 
-	},
-		{
+	}
+
+var snake =	{
 		image: 'http://clipartsign.com/upload/2016/01/27/cartoon-snakes-clip-art-page-2-snake-images-clipart-free-clip.png',
 		choices_for_answer: [ {label:'K', correct: false}, {label: 'C', correct: false}, {label: 'S', correct: true}, {label: 'D', correct: false}]
-	},
-		{
+	}
+
+var monkey = {
 		image: 'http://vignette2.wikia.nocookie.net/headsoccer/images/b/b8/Cartoon-monkey_6.png/revision/latest?cb=20150814115806',
 		choices_for_answer: [ {label:'J', correct: false}, {label: 'N', correct: false}, {label: 'A', correct: false}, {label: 'M', correct: true}]
 
-	},
-		{
+	}
+
+var kangaroo =	{
 		image: 'http://www.clipartonline.net/_/rsrc/1434566857092/Kangaroo-Cartoon-Images/kangaroo-with-joey%201.png?height=320&width=320',
 		choices_for_answer: [ {label:'K', correct: true}, {label: 'R', correct: false}, {label: 'Y', correct: false}, {label: 'O', correct: false}]
 
-	},
-		{
+	}
+
+var cat =	{
 		image: 'http://cat-pictures.clipartonline.net/_/rsrc/1359117771956/cute-kittens-images-page-2/cat-image%2010.png?height=320&width=320',
 		choices_for_answer: [ {label:'B', correct: false}, {label: 'U', correct: false}, {label: 'C', correct: true}, {label: 'G', correct: false}]
 	}
-];
 
-export default {questions};
+
+ export default [
+ 	turtle,
+ 	elephant,
+ 	zebra,
+ 	lion,
+ 	snake,
+ 	monkey,
+ 	kangaroo,
+ 	cat
+ ]
+ 
